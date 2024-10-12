2843f6b88ea51cad384bfd3bc3d1ae78c6d2f366
yqc <yqc1236@qq.com>
2024-10-13 00:23:40 +0800

    IO流代码及练习代码

---
+++ day28/src/Test/Student.java
@@ -0,0 +1,70 @@
+package Test;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 8986009981186061115L;
+    private String name;
+    private int age;
+    private String address;
+
+    public Student(String name, int age, String address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    public Student() {
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * 获取
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * 设置
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * 获取
+     * @return age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * 设置
+     * @param age
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
