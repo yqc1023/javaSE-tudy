5eb858c70fce8d4aa7e38e3942cac5552e9d66b7
yqc <yqc1236@qq.com>
2024-10-15 21:19:27 +0800

    双列集合

---
+++ day25/src/Stream流/双列.java
@@ -0,0 +1,24 @@
+package Stream流;
+
+import java.util.HashMap;
+
+public class 双列 {
+    public static void main(String[] args) {
+        //双列集合      无                                                   无法直接使用stream流
+        //得转换为单列集合
+
+        //1.创建双列集合
+        HashMap<String,Integer> hm = new HashMap<>();
+        //2.添加数据
+        hm.put("aaa",111);
+        hm.put("bbb",222);
+        hm.put("ccc",333);
+        hm.put("ddd",444);
+
+        //3.第一种获取stream流
+        //hm.keySet().stream().forEach(s -> System.out.println(s));
+
+        //4.第二种获取stream流
+        hm.entrySet().stream().forEach(s-> System.out.println(s));
+    }
+}
